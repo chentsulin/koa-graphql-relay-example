a53c26cc02833c8cc1be7c2fa461beb019ad5cdf
C.T. Lin <chentsulin@gmail.com>
2016-04-20 14:50:17 +0800

    Added TodoMVC example

---
+++ TodoMVC/server.js
@@ -0,0 +1,15 @@
+import koa from 'koa';
+import graphQLHTTP from 'koa-graphql';
+import mount from 'koa-mount';
+import { schema } from './data/schema';
+
+const GRAPHQL_PORT = 8080;
+
+// Expose a GraphQL endpoint
+var graphQLServer = koa();
+
+graphQLServer.use(mount('/', graphQLHTTP({ schema, pretty: true })));
+
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(
+  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
+));
